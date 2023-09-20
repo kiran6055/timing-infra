7b272e07bad629c8c6419ae0a08a8f878425bcb7
kiran6055 <kirandevopskumar@gmail.com>
2023-09-20 12:25:42 +0530

    all

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -12,14 +12,14 @@ variable "vpc_cidr" {
 }
 
 variable "public_cidr" {
-  default = "10.0.1.0/24" 
+  default = ["10.0.1.0/24", "10.0.2.0/24"] 
 }
 
 variable "private_cidr" {
-  default = "10.0.2.0/24"
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 
 variable "database_cidr" {
-  default = "10.0.3.0/24"
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
 }
