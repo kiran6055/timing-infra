5bc4dcfa7db8b54b1ef1e789ed6d11c26fad586d
kiran6055 <kirandevopskumar@gmail.com>
2023-09-20 11:52:33 +0530

    a

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "../../terraform-modules/vpc"
+  vpc_cidr = var.vpc_cidr
   public_cidr = var.public_cidr
   private_cidr = var.private_cidr
   database_cidr = var.databse_cidr
