42d09280cb00b70ed0b7d6991746816194e7a38f
root <root@ip-172-31-40-113.ap-south-1.compute.internal>
2024-03-05 10:21:16 +0000

    al

--- variables.tf
+++ variables.tf
@@ -112,11 +112,11 @@ variable "instance_type" {
 }
 
  variable "secret_name" {
-   default = "timing/rds-password-value9"
+   default = "timing/rds-password-value10"
  }
 
 variable "rds_secret_arn" {
-  default = "arn:aws:secretsmanager:ap-south-1:358308582535:secret:timing/rds-password-value9-1hJ4iJ"
+  default = "arn:aws:secretsmanager:ap-south-1:358308582535:secret:timing/rds-password-value10-M6VZYt"
 }
 
 variable "ecs_cluster_name" {
